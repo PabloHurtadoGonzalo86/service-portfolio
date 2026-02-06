b1de2738ea6a0bfe1b1aa6883eed66daffaa23b0
PabloHurtadoGonzalo <pablofullstackdevelopernrby@gmail.com>
2026-02-06 14:58:18 +0100

    Enable OAuth2 login with GitHub and update application.yaml configuration

--- src/main/kotlin/com/example/serviceportfolio/controller/HomeController.kt
+++ src/main/kotlin/com/example/serviceportfolio/controller/HomeController.kt
@@ -2,6 +2,7 @@ package com.example.serviceportfolio.controller
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @CrossOrigin(origins = ["*"]) // TODO: Restrict to specific origins in production
 class HomeController {
 
-    @RequestMapping("/hello")
+    @GetMapping
     fun home(): ResponseEntity<String> {
         return ResponseEntity.ok("Service Portfolio API")
     }
