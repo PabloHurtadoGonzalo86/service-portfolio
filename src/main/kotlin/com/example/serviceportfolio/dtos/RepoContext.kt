7b1dfe02c4d54dc526a547f6f9d56f77fcf88ecb
PabloHurtadoGonzalo <pablofullstackdevelopernrby@gmail.com>
2026-02-06 15:16:12 +0100

    Make `description` and `language` fields in `RepoContext` optional.

--- src/main/kotlin/com/example/serviceportfolio/dtos/RepoContext.kt
+++ src/main/kotlin/com/example/serviceportfolio/dtos/RepoContext.kt
@@ -5,5 +5,7 @@ data class RepoContext(
     val description: String?,
     val language: String?,
     val languages: Map<String, Long>,
+    val fileTree: List<String>,        // estructura de archivos
+    val readmeContent: String?,        // README actual si existe
     val keyFiles: Map<String, String>,
 )
