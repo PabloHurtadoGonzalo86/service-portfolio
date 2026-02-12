93263b443366910d6906de69f09e52c32b6d5041
PabloHurtadoGonzalo <pablofullstackdevelopernrby@gmail.com>
2026-02-12 12:41:48 +0100

    Add `RepoAnalysis` data class to represent repository analysis details

---
+++ src/main/kotlin/com/example/serviceportfolio/models/RepoAnalysis.kt
@@ -0,0 +1,9 @@
+package com.example.serviceportfolio.models
+
+data class RepoAnalysis(
+    val projectName: String,
+    val shortDescription: String,
+    val techStack: List<String>,
+    val detectedFeatures: List<String>,
+    val readmeMarkdown: String? = null
+)
